25929a90a904b60db265622fec4afbdc86189e22
VGumenyuk1 <vgumenuk@gmail.com>
2024-09-11 22:54:45 +0200

    HW2.1 added

--- pythonProject1/HW 2.1.py
+++ pythonProject1/HW 2.1.py
@@ -1,12 +1,12 @@
 # Просимо користувача ввести 4-значне число
 number = int(input("Введіть 4-значне число: "))
 
-# Отримуємо цифри числа за допомогою операцій ділення
+
 thousands, remainder = divmod(number, 1000)
 hundreds, remainder = divmod(remainder, 100)
 tens, ones = divmod(remainder, 10)
 
-# Виводимо цифри в стовпчик
+
 print(thousands)
 print(hundreds)
 print(tens)
