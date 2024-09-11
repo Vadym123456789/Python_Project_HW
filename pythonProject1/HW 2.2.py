950ed47e1c04d6f71246d4180ddedcbd2c3316f1
VGumenyuk1 <vgumenuk@gmail.com>
2024-09-11 22:33:33 +0200

    HW2.1 added

---
+++ pythonProject1/HW 2.2.py
@@ -0,0 +1,15 @@
+# Просимо користувача ввести 5-значне число
+number = int(input("Введіть 5-значне число: "))
+
+# Розділяємо число на окремі цифри
+fifth = number % 10
+fourth = (number // 10) % 10
+third = (number // 100) % 10
+second = (number // 1000) % 10
+first = number // 10000
+
+# Збираємо число у зворотньому порядку
+reversed_number = fifth * 10000 + fourth * 1000 + third * 100 + second * 10 + first
+
+# Виводимо результат
+print(reversed_number)
