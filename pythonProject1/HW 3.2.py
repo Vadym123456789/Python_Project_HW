341eff2bb92e31fbc51c4aa4fa41d0bca8ef6630
VGumenyuk1 <vgumenuk@gmail.com>
2024-09-21 12:34:16 +0200

    HW2.1 added

--- pythonProject1/HW 3.2.py
+++ pythonProject1/HW 3.2.py
@@ -1,17 +1,23 @@
-def move_last_to_first(lst):
-    if len(lst) <= 1:
-        return lst
-    return [lst[-1]] + lst[:-1]
-
-# Тестові приклади
+# Тестові випадки
 test_cases = [
-    [12, 3, 4, 10],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3],
+    [1, 2, 3, 4, 5],
     [1],
-    [],
-    [12, 3, 4, 10, 8]
+    []
 ]
 
-# Перевірка кожного тестового випадку
-for case in test_cases:
-    result = move_last_to_first(case)
-    print(f"{case} => {result}")
+# Обробка кожного тестового випадку
+for input_list in test_cases:
+    # Обчислення індексу середини списку
+    mid = len(input_list) // 2
+
+    # Розділення списку на дві частини
+    first_half = input_list[:mid]
+    second_half = input_list[mid:]
+
+    # Формування результату
+    result = [first_half, second_half]
+
+    # Виведення результату
+    print(f"{input_list} => {result}")
