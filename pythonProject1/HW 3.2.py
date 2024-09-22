354853e6f79ca4c37d80fa118c8562b40fb78a08
VGumenyuk1 <vgumenuk@gmail.com>
2024-09-22 12:01:55 +0200

    HW2.1 added

--- pythonProject1/HW 3.2.py
+++ pythonProject1/HW 3.2.py
@@ -1,23 +1,14 @@
-# Тестові випадки
 test_cases = [
-    [1, 2, 3, 4, 5, 6],
-    [1, 2, 3],
-    [1, 2, 3, 4, 5],
+    [12, 3, 4, 10],
     [1],
-    []
+    [],
+    [12, 3, 4, 10, 8]
 ]
 
-# Обробка кожного тестового випадку
-for input_list in test_cases:
-    # Обчислення індексу середини списку
-    mid = len(input_list) // 2
-
-    # Розділення списку на дві частини
-    first_half = input_list[:mid]
-    second_half = input_list[mid:]
-
-    # Формування результату
-    result = [first_half, second_half]
-
-    # Виведення результату
-    print(f"{input_list} => {result}")
+# Перевірка кожного тестового випадку
+for lst in test_cases:
+    if len(lst) <= 1:
+        result = lst
+    else:
+        result = [lst[-1]] + lst[:-1]
+    print(f"{lst} => {result}")
