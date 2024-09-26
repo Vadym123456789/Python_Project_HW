764531a47aa1db4053d4b0997dcbf344b918ecaf
VGumenyuk1 <vgumenuk@gmail.com>
2024-09-26 13:55:07 +0200

    HW2.1 added

--- pythonProject1/HW 6.2.py
+++ pythonProject1/HW 6.2.py
@@ -1,6 +1,5 @@
 # Отримуємо ввід від користувача
 seconds = int(input("Введіть кількість секунд (0-8639999): "))
-
 # Перевірка введеного значення
 if seconds < 0 or seconds > 8639999:
     print("Неправильне значення. Введіть число від 0 до 8639999.")
@@ -9,17 +8,23 @@ else:
     days, remainder = divmod(seconds, 86400)  # 86400 секунд у добі
     hours, remainder = divmod(remainder, 3600)  # 3600 секунд у годині
     minutes, seconds = divmod(remainder, 60)  # 60 секунд у хвилині
-
-    # Визначення правильної форми слова "день"
-    if days == 1:
+    # Визначення правильної форми слова "день" без використання функції
+    if 11 <= days % 100 <= 19:
+        day_word = "днів"
+    elif days % 10 == 1:
         day_word = "день"
-    elif 1 < days < 5:
+    elif 2 <= days % 10 <= 4:
         day_word = "дні"
     else:
         day_word = "днів"
-
     # Форматування результату
     result = f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
-
     # Виведення результату
     print(result)
+
+
+
+
+
+
+
